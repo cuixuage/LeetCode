2b73829e19da8137cc548c984dac672a90ca77f3
cuixuage <1157097502@qq.com>
2018-07-24 19:47:23 +0800

    sum O(N)

---
+++ HashTable/1_求和II.cpp
@@ -0,0 +1,51 @@
+/*
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+思路1:
+1.O(N^2)
+2.O(N)
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans(2,0);
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+
+            }
+        }
+        return ans;
+    }
+};
+
+//思路二
+// key:number value:index
+vector<int> twoSum(vector<int> &numbers, int target)
+{
+    //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < numbers.size(); i++) {
+		int numberToFind = target - numbers[i];
+
+            //if numberToFind is found in map, return them
+		if (hash.find(numberToFind) != hash.end()) {
+                    //+1 because indices are NOT zero based
+			result.push_back(hash[numberToFind] + 1);
+			result.push_back(i + 1);
+			return result;
+		}
+
+            //number was not found. Put it in the map.
+		hash[numbers[i]] = i;
+	}
+	return result;
+}
