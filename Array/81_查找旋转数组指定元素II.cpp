3921f7d14584e2e7725aef2321d2cb9f7a63d9b9
cuixuage <1157097502@qq.com>
2018-11-11 20:51:58 +0800

    add search from 牛客网

--- Array/81_查找旋转数组指定元素II.cpp
+++ Array/81_查找旋转数组指定元素II.cpp
@@ -1,24 +1,23 @@
 //一遍AC
-//假设没有重复元素
+//假设可能存在重复元素
 
-int search(vector<int>& nums, int target){
-    //empty更快？ 而非使用len 
-    if(nums.empty()) return -1;
+bool search(vector<int>& nums, int target) {
+    if(nums.empty()) return false;
     
     int start = nums.front();
     int end = nums.back();
     if(target < start && target > end)
-        return -1;
+        return false;
     if(target <= end){
         for(int i=nums.size()-1;i>=0;i--){
             if(nums[i] == target)
-                return i;
+                return true;
         }
     }else{
         for(int i=0;i<nums.size();i++){
             if(nums[i] == target)
-                return i;
+                return true;
         }
     }
-    return -1;
+    return false;    
 }
