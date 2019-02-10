07ec34e9b6b44234187505d40ad0ad5d72ffce63
cuixuage <1157097502@qq.com>
2019-02-10 20:41:46 +0800

    tree

---
+++ 剑指offer/23_层级打印.cpp
@@ -0,0 +1,22 @@
+/*
+按照层级打印二叉树
+本题返回值类型 vector<int>
+而60题 返回值类型是vector<vector<int>>
+*/
+
+#include "func.h"
+
+vector<int> PrintFromTopToBottom(TreeNode* root){
+    vector<int> ans;
+    if(root==NULL) return ans;
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        TreeNode* top = q.front();
+        q.pop();
+        ans.push_back(top->val);
+        if(top->left) q.push(top->left);
+        if(top->right) q.push(top->right);
+    } 
+    return ans;
+}
