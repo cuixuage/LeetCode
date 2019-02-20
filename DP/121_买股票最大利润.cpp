5a57bfa58094f64c3a88d179a37b6dbda24cec79
cuixuage <1157097502@qq.com>
2019-02-20 09:43:46 +0800

    DP update

--- DP/121_买股票最大利润.cpp
+++ DP/121_买股票最大利润.cpp
@@ -1,25 +1,31 @@
 /*
+只能买卖一次股票的情况下:
+
 给定vector寻找前后差值最大的数值，要求序号高的数字减去序号低的数字。
 错误思路: 最长递增子序列 反例:1 5 2 3 4 maxprofit应为4
+
+正确思路:
+类似DP动态规划:
+保存当前节点为止的最小值,那么profit = array[i] - currentMin;
 */
 #include "func.h"
 using std::cout;
 using std::endl;
 
-//min 记录当前元素前面所有中的最小值
-//比较 已经保存的maxprofit 和 value[i]-min的差值区别
-int maxProfit(vector<int>& prices) {
-    int valMin = INT_MAX;
-    int profit = 0;
-    for(int i=0;i<prices.size();i++){
-        if(prices[i] < valMin) valMin=prices[i];
-        profit = std::max(profit,prices[i]-valMin);
-    }
-    return profit;
-}
 
-int main(){
-    vector<int> test = {1,2,2,3,3};
-    cout<<maxProfit(test)<<endl;
-    return 0;
+//update  一次AC  要想明白逻辑
+//数组minVal存储 到当前节点为止的出现的最小股票值 那么profit = prices[i] - minVal[i]
+int maxProfit(vector<int>& prices){
+    if(prices.size()==0) return 0;
+    vector<int> minVal(prices.size(),0);
+    minVal[0] = prices[0];
+    int profit = 0;
+    for(int i=1;i<prices.size();i++){
+        if(minVal[i-1] < prices[i])
+            minVal[i] = minVal[i-1];
+        else
+            minVal[i] = prices[i];
+        profit = std::max(profit,prices[i]-minVal[i]);
+    }
+    return profit;   
 }
--- DP/122_卖股票最大利润II.cpp
+++ DP/122_卖股票最大利润II.cpp
@@ -1,20 +1,25 @@
-//正确思路:  寻找连续的递增序列的若干区间
-//所有的递增区间都是一次买入卖出的操作，最后将所有利润求和
-//一次AC
+/*
+正确思路:  
+寻找连续的递增序列的若干区间。所有的递增区间都是一次买入卖出的操作，最后将所有利润求和
+DP: profitVal[] 以当前元素为止的最大利润
+dp[i] = dp[i-1]+prices[i]-prices[i-1]    (仍处于递增区间)
+      = dp[i-1]     
+*/
+
 int maxProfit(vector<int>& prices){
     int n = prices.size();
-    if(n==0||n==1) return 0;
-    vector<int> dp(n,0);
-    int ans = 0;
+    if(n==0 || n==1) return 0;
+    vector<int> profitVal(n,0);
+    int profit = 0;
     for(int i=1;i<n;i++){
-        if(prices[i] > prices[i-1]) 
-            dp[i] = dp[i-1]+ prices[i] - prices[i-1];
-        if(dp[i]!=0)
-            ans += dp[i]-dp[i-1];
+        if(prices[i] > prices[i-1])
+            profitVal[i] = profitVal[i-1] + prices[i] - prices[i-1];        //核心DP
+        else
+            profitVal[i] = profitVal[i-1];
+        profit = std::max(profit,profitVal[i]);
     }
-    return ans;
+    return profit;
 }
-
 
 //思路1:  根据121题目 + 贪心  每次找最小最大
 //错误e.g.   [7,1,5,3,6,4] output:5  expected:7
